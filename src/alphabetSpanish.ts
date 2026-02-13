9cc6a8ff1162cd1f51bf6a3022e17b2a20d4d28b
Iv4nTech <ivangomezjimenez16@gmail.com>
2026-02-13 18:53:25 +0100

    Create component keyboard virtual

--- src/alphabetSpanish.ts
+++ src/alphabetSpanish.ts
@@ -2,6 +2,7 @@ export const alphabetSpanish = [
   [ 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
   ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ñ'],
   ['z', 'x', 'c', 'v', 'b', 'n', 'm'],
+  [' ']
 ];
 
-export const numberColumn = [0, 1, 2];
+export const numberColumn = [0, 1, 2, 3];
--- src/components/KeyboardVirtual/KeyboardVirtual.tsx
+++ src/components/KeyboardVirtual/KeyboardVirtual.tsx
@@ -1,11 +1,39 @@
 import type { JSX } from "react"
 import { alphabetSpanish, numberColumn } from "../../alphabetSpanish"
 import './KeyboardVirtual.css'
+import { useEffect } from "react";
+import { useRef } from "react";
+
 
 export const KeyboardVirtual = () => {
     
+    const keyboardRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+
+        const keypress = (event:Event) => {
+           
+            if (!keyboardRef.current) return;
+            for (const col of keyboardRef.current.children) {
+                for (const row of col.children)
+                    if (row.innerHTML === event.key) {
+                        row.classList.add('key-pressed')
+                        setTimeout(() => {
+                            row.classList.remove('key-pressed')
+                        }, 150);
+                    }
+            }
+        }
+        window.addEventListener('keydown', keypress)
+    
+      return () => {
+        window.removeEventListener('keydown', keypress)
+      }
+    }, [])
+
+
     return (
-        <div className="keyboard">
+        <div className="keyboard" ref={keyboardRef}>
             {numberColumn.map((n): JSX.Element => {
                 return (
                     <div className={`column-keys c${n+1}`} key={`row-${n}`}>
